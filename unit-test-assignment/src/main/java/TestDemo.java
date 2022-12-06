daabc7314ea68bdb6620cadc1e750f7bcf2e6d7a
Haven Summerfield <95510327+HavDevMaster@users.noreply.github.com>
2022-12-05 20:34:17 -0600

    Add files via upload

---
+++ unit-test-assignment/src/main/java/TestDemo.java
@@ -0,0 +1,29 @@
+import java.util.Random;
+
+public class TestDemo {
+
+	public static void main(String[] args) {
+		
+	}
+	public int addPositive(int a, int b) {
+		if(a > 0 && b > 0) {
+		return a + b;
+		}
+		else {
+		throw new IllegalArgumentException("Both parameters must be positive!");
+	}
+		
+  }	
+	public static int randomNumberSquared() { 
+		int randomNumber = getRandomInt();
+		int rSquared = (randomNumber * randomNumber);
+		return rSquared;
+	}
+	static int getRandomInt() {
+		Random random = new Random();
+		return random.nextInt(10) + 1;
+	}
+	
+}
+	
+	
